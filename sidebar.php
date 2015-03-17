df64e06600e378b29a574c7d16f59752f4c64878
Lars Boesenach <lars@lokaalgevonden.nl>
2015-03-17 16:39:48 +0100

    added the styling for sidebar content

--- sidebar.php
+++ sidebar.php
@@ -12,15 +12,84 @@
 		<div class="Sidebar-log">
 			<div class="Week-wrap">
 				<h2 class="Week-overview">Week overview</h2>
+				
 				<hr>
+				
 				<div class="Week-count">
 					<a class="navigator left" href="#"> < </a>
 					<h2>Week 05</h2>
 					<p>2 till 8 February</p>
 					<a class="navigator right" href="#"> > </a>
 				</div>
+				
 				<hr>
-			</div>
-		</div>
-	</div>
-</div>
+				
+				<div class="Week-output">
+					<div class="Indicators">
+						<table>
+							<tr>
+								<td class="day"><p>Day</p></td>
+								<td class="exercise"><p>Exercise</p></td>
+								<td class="kcal"><p>Kcal</p></td>
+							</tr>
+						</table>
+					</div>
+					
+					<div class="Week-content">
+						<table>
+							<tr>
+								<td class="day"><p>Monday</p></td>
+								<td class="exercise"><p>720</p></td>
+								<td class="kcal"><p>2100</p></td>
+							</tr>
+							<tr>
+								<td class="day"><p>Thuesday</p></td>
+								<td class="exercise"><p>230</p></td>
+								<td class="kcal"><p>2320</p></td>
+							</tr>
+							<tr>
+								<td class="day"><p>Wednesday</p></td>
+								<td class="exercise"><p>0</p></td>
+								<td class="kcal"><p>2050</p></td>
+							</tr>
+							<tr class="negative">
+								<td class="day"><p>Thursday</p></td>
+								<td class="exercise"><p>0</p></td>
+								<td class="kcal"><p>2610</p></td>
+							</tr>
+							<tr>
+								<td class="day"><p>Friday</p></td>
+								<td class="exercise"><p>920</p></td>
+								<td class="kcal"><p>2205</p></td>
+							</tr>
+							<tr class="empty">
+								<td class="day"><p>Saturday</p></td>
+								<td class="exercise"><p>0</p></td>
+								<td class="kcal"><p>0</p></td>
+							</tr>
+							<tr class="empty">
+								<td class="day"><p>Sunday</p></td>
+								<td class="exercise"><p>0</p></td>
+								<td class="kcal"><p>0</p></td>
+							</tr>
+						</table>
+					</div><!-- Week-content -->
+
+					<hr>
+
+					<div class="Week-average">
+						<table>
+							<tr>
+								<td class="left"><p>Average</p></td>
+								<td class="right"><p>2310</p></td>
+							</tr>
+						</table>
+					</div><!-- Week-Average -->
+
+					<hr>
+
+				</div><!-- Week-output -->
+			</div><!-- Week-wrap -->
+		</div><!-- Sidebar-log -->
+	</div><!-- Sidebar-log-wrap -->
+</div><!-- Sidebar-wrap -->

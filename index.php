467bc448f91cbc1b47172105fc0b3410876f890f
Lars Boesenach <lars@lokaalgevonden.nl>
2015-03-19 17:24:12 +0100

    Styled placehold content for the entries

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 	<div class="Content-wrap">
 		<div class="Top-bar">
 			<div class="Topbar-Logo">
-				<img src="img/Team-logo-temp.jpg">
+				<img src="img/CC_white.svg">
 				<p>Calorie Counter</p>
 			</div>
 
@@ -38,7 +38,87 @@
 
 		<div class="Content-entry">
 			<div class="Content-Logo">
-				<img src="img/Team-logo-temp.jpg">
+				<img src="img/CC_grey.svg">
+			</div>
+
+
+			<div class="Entry-contents">
+				<div class="Entry-overview">
+					<table>
+						<tr>
+							<td><p>Date</p></td>
+							<td><p>05/02/2015</p></td>
+						</tr>
+						<tr>
+							<td><p>Food Entries</p></td>
+							<td><p>4</p></td>
+						</tr>
+						<tr>
+							<td><p>Exercise Entries</p></td>
+							<td><p>2</p></td>
+						</tr>
+					</table>
+				</div><!-- Entry-overview -->
+
+
+
+				<div class="Entry-entries">
+					<table class="intake">
+						<tr>
+							<td><p>Amount</p></td>
+							<td><p>Product name</p></td>
+							<td><p>Kilo Calories (KCAL)</p></td>
+						</tr>
+						<!-- -------------- -->
+						<tr>
+							<td><p>01x</p></td>
+							<td><p>Pizza BBQ Cchicken</p></td>
+							<td><p>1150</p></td>
+						</tr>
+						<tr>
+							<td><p>02x</p></td>
+							<td><p>banana</p></td>
+							<td><p>200</p></td>
+						</tr>
+						<tr>
+							<td><p>01x</p></td>
+							<td><p>glass of milk</p></td>
+							<td><p>60</p></td>
+						</tr>
+						<tr>
+							<td><p>03x</p></td>
+							<td><p>special K bar</p></td>
+							<td><p>150</p></td>
+						</tr>
+
+					</table>
+				</div><!-- values -->
+
+				<div class="Entry-entries">
+					<table class="fitness">
+						<tr>
+							<td><p>Minutes</p></td>
+							<td><p>Intensity</p></td>
+							<td><p>Workout name</p></td>
+							<td><p>Kilo Calories (KCAL)</p></td>
+						</tr>
+						<!-- -------------- -->
+						<tr>
+							<td><p>30</p></td>
+							<td><p>medium</p></td>
+							<td><p>running</p></td>
+							<td><p>595</p></td>
+						</tr>
+						<tr>
+							<td><p>60</p></td>
+							<td><p>hard</p></td>
+							<td><p>fitness</p></td>
+							<td><p>500</p></td>
+						</tr>
+					</table>
+				</div><!-- entries -->
+
+
 			</div>
 		</div><!-- Content-entry -->
 	</div><!-- Content-wrap -->

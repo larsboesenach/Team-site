840aec064d4cc0d55f0723a767221f92cc065820
Lars Boesenach <lars@lokaalgevonden.nl>
2015-04-23 11:48:42 +0200

    added the usersettings and goal

--- includes/UserSettings.php
+++ includes/UserSettings.php
@@ -25,10 +25,80 @@
 
 		</div>
 		
-		<div class="User-content">
+		<div class="User-content settings">
 			<div class="UserClose-button">
 				<p>Close Profile</p>
 				<img src="img/dark_kruisje-2.svg">
+			</div>
+
+			<div class="Content">
+				<div class="Title"><h2>Settings</h2></div>
+				
+				<div class="Input-item">
+					<p>Change user name</p>
+					<input class="check" placeholder="Name">
+				</div>
+
+				<div class="Input-item">
+					<p>Change email adress</p>
+					<input class="check" placeholder="Email">
+				</div>
+
+				<div class="Input-item">
+					<p>Change password</p>
+					<input class="check" placeholder="Current password">
+					<input class="check" placeholder="New password">
+				</div>
+			</div>
+
+			<div class="UserSettings-button update">
+				<p>Update</p>
+			</div>
+		</div>
+
+
+		<div class="User-content goal">
+			<div class="UserClose-button">
+				<p>Close Profile</p>
+				<img src="img/dark_kruisje-2.svg">
+			</div>
+
+			<div class="Content">
+				<div class="Title"><h2>Goal</h2></div>
+				
+				<div class="Input-item">
+					<p>Please select gender</p>
+					<input class="check" placeholder="gender">
+				</div>
+
+				<div class="Input-item">
+					<p>Fill in your weight in kg</p>
+					<input class="check" placeholder="0kg">
+				</div>
+
+				<div class="Input-item">
+					<p>Fill in height in meters</p>
+					<input class="check" placeholder="Current password">
+				</div>
+
+				<div class="Input-item">
+					<p>Activity based on a regular day</p>
+					<input class="check" placeholder="Current password">
+				</div>
+
+				<div class="Input-item">
+					<p>Activity your goal</p>
+					<input class="check" placeholder="Current password">
+				</div>
+
+				<div class="Input-item">
+					<p>Your goal</p>
+					<input class="check" placeholder="2500 kcal">
+				</div>
+			</div>
+
+			<div class="UserSettings-button update">
+				<p>Let's Go!</p>
 			</div>
 		</div>
 
